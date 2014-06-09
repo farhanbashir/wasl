06cdc819ca48b87c11c6494057f1cdd2ec860716
muhammad farhan bashir <farhan.bashir2002@gmail.com>
2014-06-10 00:53:26 +0500

    new functions added

--- index.php
+++ index.php
@@ -1,116 +1,246 @@
 <?php
-/**
- * Step 1: Require the Slim Framework
- *
- * If you are not using Composer, you need to require the
- * Slim Framework and register its PSR-0 autoloader.
- *
- * If you are using Composer, you can skip this step.
- */
+
 error_reporting(E_ALL); 
  
 require "config.php"; 
 require "functions.php"; 
-require "NotORM.php";
+//require "NotORM.php";
 
-$pdo = new PDO("mysql:host=".$config["db"]["db_host"].";dbname=".$config["db"]["db_name"], $config["db"]["db_user"], $config["db"]["db_password"]);
-$db = new NotORM($pdo);
+$db = new PDO("mysql:host=".$config["db"]["db_host"].";dbname=".$config["db"]["db_name"], $config["db"]["db_user"], $config["db"]["db_password"]);
+//$db = new NotORM($pdo);
 
 require 'Slim/Slim.php';
 
 \Slim\Slim::registerAutoloader();
 
-/**
- * Step 2: Instantiate a Slim application
- *
- * This example instantiates a Slim application using
- * its default settings. However, you will usually configure
- * your Slim application now by passing an associative array
- * of setting names and values into the application constructor.
- */
+
 $app = new \Slim\Slim(array("MODE" => "development"));
 
 $response = array();
 
-/**
- * Step 3: Define the Slim application routes
- *
- * Here we define several Slim application routes that respond
- * to appropriate HTTP request methods. In this example, the second
- * argument for `Slim::get`, `Slim::post`, `Slim::put`, `Slim::patch`, and `Slim::delete`
- * is an anonymous function.
- */
+$app->get('/users','getUsers');
+$app->get("/login/:username/:password/:type",'login');
+$app->get("/getProfile/:username",'getProfile');
+$app->post('/signup','signup');
 
-// GET route
-$app->get(
-    '/',
-    function () {
-        
-        echo "<h1>It's a Slim World</h1>";
+$app->post('/editProfile','editProfile');
+$app->post('/updatePassword','updatePassword');
+$app->post('/forgotPassword','forgotPassword');
+$app->get("/getMyEvents/:user_id/:event_id",'getMyEvents');
+$app->get("/searchEventByName/:search",'searchEventByName');
+$app->get("/searchEventByLocation/:latitude/:longitude",'searchEventByLocation');
+$app->get("/getEventFeedByEventId/:user_id/:event_id",'getEventFeedByEventId');
+$app->get("/getEventUserList/:event_id",'getEventUserList');
+$app->post("/joinThisEvent",'joinThisEvent');
+$app->post("/checkedInThisEvent",'checkedInThisEvent');
+$app->post("/postStatusOnEvent",'postStatusOnEvent');
+
+
+function getUsers()
+{
+	global $app ,$db, $response;
+	$users = array();
+    
+    $sql = "SELECT * FROM users";
+    
+    try{
+        $stmt   = $db->query($sql);
+        $users  = $stmt->fetchAll(PDO::FETCH_NAMED);
+        $response["header"]["error"] = 0;
+        $response["header"]["message"] = "Success";
     }
-);
-
-$app->get("/users", function () use ($app, $db, $response) {
-    $users = array();
-    foreach ($db->users() as $user) {
-        $users[]  = array(
-            "id" => $user["id"],
-            "first_name" => $user["first_name"],
-            "last_name" => $user["last_name"]
-
-        );
+    catch(PDOException $e){
+        $response["header"]["error"] = 1;
+        $response["header"]["message"] = $e->getMessage();
     }
     
-    $response["header"]["error"] = 0;
-    $response["header"]["message"] = "Success";
+    
+    
     $response["body"] = $users;
     
     $app->response()->header("Content-Type", "application/json");
     echo json_encode($response);
-});
+	
+	
+}
 
-$app->get("/getInfo/:username",function($username) use ($app,$db){
+
+function getProfile($username){
+	global $app,$db;
     $info = array();
-    $user = $db->users->where("username = ?",$username);
-    $app->response()->header("Content-Type", "application/json");
-    echo json_encode($user);
-
-});
-
-$app->get("/authenticate/:username/:password",function($username,$password) use ($app,$db,$response){
-    $data = array();
     
-
-    if($user = $db->users->where("username",$username)->fetch())
-    {
+    $sql = "SELECT * FROM users where username=:username ";
+    try{
+        $stmt = $db->prepare($sql);  
+        $stmt->bindParam("username", $username);
+        $stmt->execute();
+        //$stmt   = $db->query($sql);
+        $info  = $stmt->fetch(PDO::FETCH_NAMED);
+        
         $response["header"]["error"] = 0;
         $response["header"]["message"] = "Success";
-        $data["info"] = $user;
-            
+        
     }
-    else
-    {
-        $response["header"]["error"] = 0;
-        $response["header"]["message"] = "Fail";
-    }        
+    catch(PDOException $e){
+        $response["header"]["error"] = 1;
+        $response["header"]["message"] = $e->getMessage();
+    }    
     
-    
-    $response["header"]["error"] = 0;
-    $response["header"]["message"] = "Success";
-    $response["body"] = $data;
-    
-    
+    $response["body"] = $info;
+        
     $app->response()->header("Content-Type", "application/json");
     echo json_encode($response);
 
-});
+}
 
+function login($username,$password, $type){
+    global $app, $db, $response;	
+    $data = array();
+    
+
+    $sql = "SELECT * FROM users where username=:username ";
+    try{
+        $stmt = $db->prepare($sql);  
+        $stmt->bindParam("username", $username);
+        $stmt->execute();
+        //$stmt   = $db->query($sql);
+        $data  = $stmt->fetch(PDO::FETCH_NAMED);
+        
+        if(count($data))
+        {
+        	if($type == 0)
+			{
+	            if($data["password"] == MD5($password))
+	            {
+	                $response["header"]["error"] = 0;
+	                $response["header"]["message"] = "Success";
+	            }
+	            else
+	            {
+	                $data = array();
+	                $response["header"]["error"] = 1;
+	                $response["header"]["message"] = "Username or password incorrect";    
+	            }
+			}
+			else {
+				$response["header"]["error"] = 0;
+	            $response["header"]["message"] = "Success";
+			}	    
+        }
+        else
+        {
+            $response["header"]["error"] = 1;
+            $response["header"]["message"] = "No user found";
+        }
+        
+        
+    }
+    catch(PDOException $e){
+        $response["header"]["error"] = 1;
+        $response["header"]["message"] = $e->getMessage();
+    }    
+    
+    $response["body"] = $data;
+        
+    $app->response()->header("Content-Type", "application/json");
+    echo json_encode($response);
+
+}
+
+function signup() {
+	global $app, $db, $response;
+	$user = array("user_id"=>0);
+	
+	$req = $app->request(); // Getting parameter with names
+    $first_name = $req->params('first_name'); // Getting parameter with names
+    $last_name = $req->params('last_name'); // Getting parameter with names
+    $username = $req->params('username'); // Getting parameter with names
+    $password = md5($req->params('password')); // Getting parameter with names
+    //debug(checkUser($username),1);
+    
+    $sql = "INSERT INTO users (first_name,last_name,username,password) values (:first_name,:last_name,:username,:password)";
+	
+	try{
+		$stmt = $db->prepare($sql);  
+        $stmt->bindParam("first_name", $first_name);
+		$stmt->bindParam("last_name", $last_name);
+		$stmt->bindParam("username", $username);
+		$stmt->bindParam("password", $password);
+        $stmt->execute();
+		
+		$user["user_id"] = $db->lastInsertId();
+        
+		$response["header"]["error"] = 0;
+        $response["header"]["message"] = "Success";
+		
+	}
+	catch(PDOException $e)
+	{
+		$response["header"]["error"] = 1;
+        $response["header"]["message"] = $e->getMessage();
+	}
+    
+    $response["body"] = $user;
+        
+    $app->response()->header("Content-Type", "application/json");
+    echo json_encode($response);
+	
+}
+
+
+function checkUser($username)
+{
+	global $db;
+	$sql = "SELECT * FROM users WHERE username=:username limit 1";
+	
+	try{
+		$stmt = $db->prepare($sql);  
+        $stmt->bindParam("username", $username);
+		$result = $stmt->execute();
+		$info  = $stmt->fetch(PDO::FETCH_NAMED);
+		
+		if(count($info) > 0)
+		{
+			debug(count($info));
+			return false;	
+		}
+		else {debug("true");
+			return true;
+		}
+	}
+	catch(PDOException $e)
+	{
+		//debug($e->getMessage(),1);
+		return false;	
+	}
+}
+
+
+function getMyEvents($user_id, $event_id = "")
+{
+    global $app, $db, $response;
+}
+
+function searchEventByName($search)
+{
+    global $app, $db, $response;
+}
+
+function searchEventByLocation($latitude, $longitude)
+{
+    global $app, $db, $response;
+}    
 
 
 // POST route
 $app->post(
     '/post',
     function () {
+    	
+		$req = $app->request(); // Getting parameter with names
+    $paramName = $req->params('name'); // Getting parameter with names
+    $paramEmail = $req->params('email'); // Getting parameter with names
+		
         echo 'This is a POST route';
     }
 );

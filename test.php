c8b9d9b7a418e3da28bc6b6cff825885fabe4625
unknown <fbashir@farhan.folio3pk.com>
2014-07-02 05:30:31 -0700

    testing files

---
+++ test.php
@@ -0,0 +1,20 @@
+<form method="post" enctype="multipart/form-data" action="http://localhost/wasl/index.php/createEvent">
+<table width="100%">
+    <tr>
+        <td>Name: </td>
+        <td><input type="text" id="name" name="name" /></td>
+    </tr>
+    <tr>
+        <td>Description: </td>
+        <td><input type="text" id="description" name="description" /></td>
+    </tr>
+    <tr>
+        <td>file: </td>
+        <td><input type="file" id="file" name="file" /></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><input type="submit" id="submit" name="submit" value="go" /></td>
+    </tr>
+</table>
+</form>
